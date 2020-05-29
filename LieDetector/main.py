92e4e0adc17dad952aeb4b8344312253343896b7
2010psw <50092524+2010psw@users.noreply.github.com>
2020-05-29 12:27:00 +0900

    Flask
    .

--- LieDetector/main.py
+++ LieDetector/main.py
@@ -1,3 +1,31 @@
-import DB.conn as db
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+############페이지############
+@app.route('/')
+def index_page():
+    return render_template('index.html')
+
+@app.route('/main')
+def main_page():
+    return render_template('main.html')
+
+#############################
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
 
 
